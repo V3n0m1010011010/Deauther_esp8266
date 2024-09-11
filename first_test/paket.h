6c8471a9811c5ad8aa1a45e654605a99a3ed8a42
V3n0m1010011010 <116117724+V3n0m1010011010@users.noreply.github.com>
2024-09-12 00:05:46 +0200

    Update paket.h

--- first_test/paket.h
+++ first_test/paket.h
@@ -6,8 +6,4 @@ uint8_t deauthPacket[26] = {
       /* 16 - 21 */ 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, //Indentifizierungs-Adresse
       /* 22 - 23 */ 0x00, 0x01, //Sequenz-nummer
       /* 24 - 25 */ 0x01, 0x00 //Reason-code
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> bbe58e329e037081869622cc17c492fd1640deed
